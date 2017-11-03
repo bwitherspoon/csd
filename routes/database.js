f4c48599c0d68eafa0fd9dc1ae24e11d05eddacc
Brett Witherspoon <spoonb@cdspooner.com>
2017-11-03 03:24:40 -0500

    Add a placeholder database view

--- routes/database.js
+++ routes/database.js
@@ -2,8 +2,10 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', function (req, res) {
-  res.render('database')
+router.get('/*', function (req, res) {
+  res.render('database', {
+    path: req.path
+  })
 })
 
 module.exports = router
