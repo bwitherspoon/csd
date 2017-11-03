fdeae3fa303d0dfc655a0d168e8065bf865990ae
Brett Witherspoon <spoonb@cdspooner.com>
2017-11-02 20:38:47 -0500

    Add placeholder views for all routes

--- app.js
+++ app.js
@@ -11,4 +11,24 @@ app.get('/', function (req, res) {
   res.render('index')
 })
 
+app.get('/about', function (req, res) {
+  res.render('about')
+})
+
+app.get('/modules', function (req, res) {
+  res.render('modules')
+})
+
+app.get('/database', function (req, res) {
+  res.render('database')
+})
+
+app.get('/resources', function (req, res) {
+  res.render('resources')
+})
+
+app.get('/contact', function (req, res) {
+  res.render('contact')
+})
+
 app.listen(8080)
