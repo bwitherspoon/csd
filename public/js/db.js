aeac3c1b1ca2257f4357f677add45e50c4d2222b
Brett Witherspoon <spoonb@cdspooner.com>
2017-11-05 22:41:40 -0600

    Add search button on database page

--- public/js/db.js
+++ public/js/db.js
@@ -1,4 +1,5 @@
-function query(elem) {
+function search() {
+  var elem = document.getElementById('select-search')
   var url = window.location.origin + window.location.pathname
   if (!url.endsWith('/')) url = url.concat('/')
   window.location.href = url + elem.value
