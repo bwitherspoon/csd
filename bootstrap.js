30c8b81b2205688ae2a38474d10da3302dc90e20
Brett Witherspoon <spoonb@cdspooner.com>
2017-11-09 17:35:28 -0600

    Fix bootstrap for previous commit

--- bootstrap.js
+++ bootstrap.js
@@ -5,7 +5,7 @@ const db = require('./database')
 
 db.connect(process.env.MONGODB_URI, function (err) {
   const user = new User(process.env.CSD_USERNAME, process.env.CSD_PASSWORD)
-  user.save(function (err) {
+  user.add(function (err) {
     if (err) {
       console.error(err.stack)
       process.exit(1)
