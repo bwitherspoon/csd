31c71cfc699442102fb3b589dd6b921cdcca6b81
Brett Witherspoon <spoonb@cdspooner.com>
2017-11-09 14:27:09 -0600

    Add initialization script

---
+++ bootstrap.js
@@ -0,0 +1,16 @@
+const assert = require('assert')
+
+const User = require('./models/user')
+const db = require('./database')
+
+db.connect(process.env.MONGODB_URI, function (err) {
+  const user = new User(process.env.CSD_USERNAME, process.env.CSD_PASSWORD)
+  user.save(function (err) {
+    if (err) {
+      console.error(err.stack)
+      process.exit(1)
+    } else {
+      process.exit()
+    }
+  })
+})
