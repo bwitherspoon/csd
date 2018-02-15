0bcb3fd59760f728b70fd4c404e47e3028cdc8a4
Brett Witherspoon <spoonb@cdspooner.com>
2018-02-14 19:00:30 -0600

    Add incomplete work an path selection

--- client/Path.js
+++ client/Path.js
@@ -77,28 +77,57 @@ class Path extends Component {
       form: ''
     }
 
-    this.onResinButtonClick = this.onResinButtonClick.bind(this)
-    this.onReinforcementButtonClick = this.onReinforcementButtonClick.bind(this)
-    this.onFormButtonClick = this.onFormButtonClick.bind(this)
+    this.handleResinButtonClick = this.handleResinButtonClick.bind(this)
+    this.handleResinItemClick = this.handleResinItemClick.bind(this)
+
+    this.handleReinforcementButtonClick = this.handleReinforcementButtonClick.bind(this)
+    this.handleReinforcementItemClick = this.handleReinforcementItemClick.bind(this)
+
+    this.handleFormButtonClick = this.handleFormButtonClick.bind(this)
+    this.handleFormItemClick = this.handleFormItemClick.bind(this)
+
+    this.handleSelectButtonClick = this.handleSelectButtonClick.bind(this)
   }
 
-  onResinButtonClick() {
+  handleResinButtonClick() {
     this.setState({resin: (this.state.resin == '' ? ',' : '')})
   }
 
-  onReinforcementButtonClick() {
+  handleResinItemClick(path, name) {
+    this.setState({resin: path + name})
+  }
+
+  handleReinforcementButtonClick() {
     this.setState({reinforcement: (this.state.reinforcement == '' ? ',' : '')})
   }
 
-  onFormButtonClick() {
+  handleReinforcementItemClick(path, name) {
+    this.setState({reinforcement: path + name})
+  }
+
+  handleFormButtonClick() {
     this.setState({form: (this.state.form == '' ? ',' : '')})
   }
 
-  toListGroupItems(mapping) {
+  handleFormItemClick(path, name) {
+    this.setState({form: path + name})
+  }
+
+  handleSelectButtonClick() {
+    const resin = this.state.resin ? this.state.resin : ','
+    const reinforcement = this.state.reinforcement ? this.state.reinforcement : ','
+    const form = this.state.form ? this.state.form : ','
+    const path = ',resin' + resin + ',reinforcement' + reinforcement + ',form' + form
+    console.log(path)
+  }
+
+  toListGroupItems(path, mapping, handler) {
     const items = []
     for (const name in mapping) {
       items.push(
-        <ListGroupItem key={name} tag="button" action>{mapping[name]}</ListGroupItem>
+        <ListGroupItem key={name} tag="button" action  onClick={() => handler(path, name)} active={false}>
+          {mapping[name]}
+        </ListGroupItem>
       )
     }
     return items
@@ -106,72 +135,87 @@ class Path extends Component {
 
   render() {
     return (
-      <Row>
-        <Col>
-          <Button color="primary" size="lg" block className="mb-3" onClick={this.onResinButtonClick} active={this.state.resin != ''}>Resin</Button>
-          {/^,(thermoplastic)?$/.test(this.state.resin) &&
-            <div>
-              <h3>Thermoplastic</h3>
-              <ListGroup className="mb-3">
-                {this.toListGroupItems(resin.thermoplastic)}
-              </ListGroup>
-            </div>
-          }
-          {this.state.resin == ',thermoplastic,polyamide,' &&
-            <div>
-              <h3>Polyamide</h3>
-              <ListGroup className="mb-3">
-                {this.toListGroupItems(resin.polyamide)}
-              </ListGroup>
-            </div>
-          }
-          {this.state.resin == ',thermoplastic,pe,' &&
-            <div>
-              <h3>Polyethylene</h3>
-              <ListGroup className="mb-3">
-                {this.toListGroupItems(resin.polyethylene)}
-              </ListGroup>
-            </div>
-          }
-          {/^,(thermoset)?$/.test(this.state.resin) &&
-            <div>
-              <h3>Thermoset</h3>
-              <ListGroup className="mb-3">
-                {this.toListGroupItems(resin.thermoset)}
-              </ListGroup>
-            </div>
-          }
-        </Col>
-        <Col>
-          <Button color="primary" size="lg" block className="mb-3" onClick={this.onReinforcementButtonClick} active={this.state.reinforcement != ''}>Reinforcement</Button>
-          {/^,(fiber)?$/.test(this.state.reinforcement) &&
-            <div>
-              <h3>Fiber</h3>
-              <ListGroup className="mb-3">
-                {this.toListGroupItems(reinforcement.fiber)}
-              </ListGroup>
-            </div>
-          }
-          {/^,(particulate)?$/.test(this.state.reinforcement) &&
-            <div>
-              <h3>Particulate</h3>
-              <ListGroup className="mb-3">
-                {this.toListGroupItems(reinforcement.particulate)}
-              </ListGroup>
-            </div>
-          }
-        </Col>
-        <Col>
-          <Button color="primary" size="lg" block className="mb-3" onClick={this.onFormButtonClick} active={this.state.form != ''}>Form</Button>
-          {this.state.form &&
-            <div>
-              <ListGroup>
-                {this.toListGroupItems(form)}
-              </ListGroup>
-            </div>
-          }
-        </Col>
-      </Row>
+      <div>
+        <Row>
+          <Col>
+            <Button color="primary" size="lg" block className="mb-3" onClick={this.handleResinButtonClick} active={this.state.resin != ''}>Resin</Button>
+
+            {/^,($|thermoplastic,)/.test(this.state.resin) &&
+              <div>
+                <h3>Thermoplastic</h3>
+                <ListGroup className="mb-3">
+                  {this.toListGroupItems(',thermoplastic,', resin.thermoplastic, this.handleResinItemClick)}
+                </ListGroup>
+              </div>
+            }
+            {/^,thermoplastic,polyamide/.test(this.state.resin) &&
+              <div>
+                <h3>Polyamide</h3>
+                <ListGroup className="mb-3">
+                  {this.toListGroupItems(',thermoplastic,polyamide,', resin.polyamide, this.handleResinItemClick)}
+                </ListGroup>
+              </div>
+            }
+            {/^,thermoplastic,pe/.test(this.state.resin) &&
+              <div>
+                <h3>Polyethylene</h3>
+                <ListGroup className="mb-3">
+                  {this.toListGroupItems(',thermoplastic,pe,', resin.polyethylene, this.handleResinItemClick)}
+                </ListGroup>
+              </div>
+            }
+            {/^,($|thermoset,)/.test(this.state.resin) &&
+              <div>
+                <h3>Thermoset</h3>
+                <ListGroup className="mb-3">
+                  {this.toListGroupItems(',thermoset,', resin.thermoset, this.handleResinItemClick)}
+                </ListGroup>
+              </div>
+            }
+          </Col>
+          <Col>
+            <Button color="primary" size="lg" block className="mb-3" onClick={this.handleReinforcementButtonClick} active={this.state.reinforcement != ''}>Reinforcement</Button>
+
+            {/^,(fiber,)?/.test(this.state.reinforcement) &&
+              <div>
+                <h3>Fiber</h3>
+                <ListGroup className="mb-3">
+                  {this.toListGroupItems(',fiber,', reinforcement.fiber, this.handleReinforcementItemClick)}
+                </ListGroup>
+              </div>
+            }
+            {/^,(particulate,)?/.test(this.state.reinforcement) &&
+              <div>
+                <h3>Particulate</h3>
+                <ListGroup className="mb-3">
+                  {this.toListGroupItems(',particulate,', reinforcement.particulate, this.handleReinforcementItemClick)}
+                </ListGroup>
+              </div>
+            }
+          </Col>
+          <Col>
+            <Button color="primary" size="lg" block className="mb-3" onClick={this.handleFormButtonClick} active={this.state.form != ''}>Form</Button>
+
+            {this.state.form &&
+              <div>
+                <ListGroup className="mb-3">
+                  {this.toListGroupItems(',', form, this.handleFormItemClick)}
+                </ListGroup>
+              </div>
+            }
+          </Col>
+        </Row>
+
+        {(this.state.resin || this.state.reinforcement || this.state.form) &&
+          <Row>
+            <Col>
+              <div className="d-flex flex-row-reverse">
+                <Button color="primary" size="lg" block className='mb-3' onClick={this.handleSelectButtonClick}>Select</Button>
+              </div>
+            </Col>
+          </Row>
+        }
+      </div>
     );
   }
 }
