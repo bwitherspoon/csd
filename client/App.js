7ca1d2d5173e767fbc2a490182e27af6e713137f
Brett Witherspoon <spoonb@cdspooner.com>
2018-02-18 17:50:13 -0600

    Add client side support for JSON POST API

--- client/App.js
+++ client/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
-import { Container } from 'reactstrap'
+import { Container, Alert } from 'reactstrap'
 import ScrapForm from './components/ScrapForm'
 import ScrapView from './components/ScrapView'
 
 class Header extends Component {
   render() {
     return (
-      <header className="header">
-        <img src='/images/logo.svg' alt='Composites Recycling' width='64px' height='64px'/>
+      <header className="p-2">
+        <img width='64px' height='64px' src='/images/logo.svg' alt='Composites Recycling' />
       </header>
     )
   }
@@ -18,6 +18,7 @@ class App extends Component {
     super(props)
     this.state = {
       view: false,
+      status: '',
       data: ScrapForm.defaultState,
     }
     this.handleView = this.handleView.bind(this)
@@ -26,28 +27,50 @@ class App extends Component {
   }
 
   handleView(data) {
-    this.setState({ view: true, data: data })
+    this.setState({
+      view: true,
+      status: '',
+      data: data
+    })
   }
 
   handleEdit(data) {
-    this.setState({ view: false })
+    this.setState({
+      view: false,
+      status: ''
+    })
   }
 
   handleSave(data) {
-    // TODO
+    fetch('/scrap', {
+      method: 'POST',
+      body: JSON.stringify(this.state.data),
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        'credentials': 'same-origin',
+      })
+    })
+    .then(res => {
+      this.setState({ status: res.statusText })
+    })
+    .catch(err => console.error('Error:', err))
   }
 
   render() {
     let body = null
     if (this.state.view) {
-      body = <ScrapView onEdit={this.handleEdit} onSave={this.onSave}
+      body = <ScrapView onEdit={this.handleEdit} onSave={this.handleSave}
                         {...this.state.data} />
     } else {
       body = <ScrapForm state={this.state.data} onView={this.handleView} />
     }
+    const color = this.state.status == 'OK' ? 'success' : 'danger';
     return (
       <Container>
         <Header />
+        <Alert className="m-4" color={color} isOpen={this.state.status != ''}>
+          {this.state.status}
+        </Alert>
         {body}
       </Container>
     )
