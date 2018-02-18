6f6feef1e2d5885d78c95077f81547a3eb93cd5f
Brett Witherspoon <spoonb@cdspooner.com>
2018-02-18 16:18:31 -0600

    Add markdown rendering

--- client/components/ScrapView.js
+++ client/components/ScrapView.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Row, Col, Button } from 'reactstrap'
+import Remarkable from 'remarkable'
 
 class ScrapView extends Component {
   constructor(props) {
@@ -10,37 +11,41 @@ class ScrapView extends Component {
   render() {
     const resin = this.props.resin ? this.props.resin.replace(/,/g, '/') : ''
     const reinforcement = this.props.reinforcement ? this.props.reinforcement.replace(/,/g, '/') : ''
+    const markdown = new Remarkable()
+    const notes = markdown.render(this.props.research_notes)
     return (
-      <div>
-        <h5 className="text-center text-muted">{(resin + ' ' + reinforcement).trim()}</h5>
-        <div className="p-4 border">
-          <Row>
-            <Col sm={{size: 8, offset: 2}}>
-              <p><strong>Form: </strong>{this.props.form}</p>
-              <p><strong>Origin Company: </strong>{this.props.origin_company}</p>
-              <p><strong>Original Material: </strong>{this.props.original_material}</p>
-              <p><strong>Manufacturing Method: </strong>{this.props.manufacturing_method}</p>
-              <p><strong>Current Location: </strong>{this.props.current_location}</p>
-              <p><strong>Quantity: </strong>{this.props.quantity}</p>
-              <p><strong>Research Notes: </strong></p>
-              <div className="notes">{/*TODO*/}</div>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Button outline color="secondary" size="lg" block
-                      onClick={this.handleEdit}>
-                Edit
-              </Button>
-            </Col>
-            <Col>
-              <Button color="primary" size="lg" block
-                      onClick={this.handleSave}>
-                Save
-              </Button>
-            </Col>
-          </Row>
-        </div>
+      <div className="p-5 m-4 border">
+        <Row>
+          <Col>
+            <p><strong>Resin: </strong>{resin}</p>
+            <p><strong>Reinforcement: </strong>{reinforcement}</p>
+            <p><strong>Form: </strong>{this.props.form}</p>
+            <p><strong>Origin Company: </strong>{this.props.origin_company}</p>
+            <p><strong>Original Material: </strong>{this.props.original_material}</p>
+            <p><strong>Manufacturing Method: </strong>{this.props.manufacturing_method}</p>
+            <p><strong>Current Location: </strong>{this.props.current_location}</p>
+            <p><strong>Quantity: </strong>{this.props.quantity} kg</p>
+            <p><strong>Research Notes:</strong></p>
+            <div className="m-4 notes" dangerouslySetInnerHTML={{ __html: notes }}/>
+          </Col>
+          <Col sm="auto">
+            <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Scrap image" />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button outline color="secondary" size="lg" block
+                    onClick={this.handleEdit}>
+              Edit
+            </Button>
+          </Col>
+          <Col>
+            <Button color="primary" size="lg" block
+                    onClick={this.handleSave}>
+              Save
+            </Button>
+          </Col>
+        </Row>
       </div>
     )
   }
