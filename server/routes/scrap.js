091bfdfeea676a373a7d59e1c7f59d24d02f001b
Brett Witherspoon <spoonb@cdspooner.com>
2018-02-18 19:24:13 -0600

    Add scrap document creation to API

--- server/routes/scrap.js
+++ server/routes/scrap.js
@@ -2,13 +2,19 @@ const express = require('express')
 const router = express.Router()
 const json = express.json()
 
+const Scrap = require('../models/scrap')
+
 router.get('/', (req, res) => {
   res.status(200).end()
 })
 
 router.post('/', json, (req, res) => {
-  console.log(req.body)
-  res.status(200).end()
+  Scrap.create(req.body, (err, doc) => {
+    if (err)
+      res.status(400).end()
+    else
+      res.status(200).end()
+  })
 })
 
 module.exports = router
