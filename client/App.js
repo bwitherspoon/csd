6fdaa09e4e908add2a75af6a822e1230eb23e74d
Brett Witherspoon <spoonb@cdspooner.com>
2018-02-26 00:24:38 -0600

    Extract scrap create page

--- client/App.js
+++ client/App.js
@@ -1,50 +1,14 @@
 import React, { Component } from 'react'
 import { Container, Alert } from 'reactstrap'
 import PageHeader from './components/PageHeader'
-import ScrapDocument from './components/ScrapDocument'
-import ScrapForm from './components/ScrapForm'
+import CreatePage from './components/CreatePage'
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      view: false,
-      status: '',
-      data: {...ScrapDocument.defaults},
+      page: 'create'
     }
-  }
-
-  handleCreate(data) {
-    this.setState({
-      view: true,
-      status: '',
-      data: data
-    })
-  }
-
-  handleCancel() {
-    this.setState({
-      view: false,
-      status: ''
-    })
-  }
-
-  handleSave() {
-    fetch('/scrap', {
-      method: 'POST',
-      body: JSON.stringify(this.state.data),
-      credentials: 'same-origin',
-      headers: new Headers({
-        'Content-Type': 'application/json',
-      }),
-      redirect: 'error',
-    })
-    .then(res => {
-      this.setState({ status: res.statusText })
-    })
-    .catch(err => {
-      this.setState({ status: err.message })
-    })
   }
 
   handleNavigate(page) {
@@ -52,29 +16,10 @@ class App extends Component {
   }
 
   render() {
-    let body = null
-    if (this.state.view) {
-      body = <ScrapDocument data={this.state.data}
-                            onCancel={this.handleCancel.bind(this)}
-                            onSave={this.handleSave.bind(this)} />
-    } else {
-      body = <ScrapForm data={this.state.data}
-                        onCreate={this.handleCreate.bind(this)} />
-    }
-    let alert = null
-    if (this.state.status) {
-      const color = this.state.status == 'OK' ? 'success' : 'danger';
-      alert = (
-        <Alert className="m-4" color={color} isOpen={this.state.status != ''}>
-          {this.state.status}
-        </Alert>
-      )
-    }
     return (
       <Container>
         <PageHeader onNavigate={this.handleNavigate.bind(this)} />
-        {alert}
-        {body}
+        <CreatePage />
       </Container>
     )
   }
