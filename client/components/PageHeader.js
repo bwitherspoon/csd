38688ad32952a2c1e646841c2343d9844ac4cf58
Brett Witherspoon <spoonb@cdspooner.com>
2018-02-26 16:11:09 -0600

    Reenable login in header

--- client/components/PageHeader.js
+++ client/components/PageHeader.js
@@ -4,10 +4,22 @@ import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import CogIcon from '@fortawesome/fontawesome-free-solid/faCog'
 import FolderIcon from '@fortawesome/fontawesome-free-solid/faFolder'
 import UserIcon from '@fortawesome/fontawesome-free-solid/faUser'
+import UserTimesIcon from '@fortawesome/fontawesome-free-solid/faUserTimes'
 
 class PageHeader extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      auth: false
+    }
+  }
+
+  componentDidMount() {
+    fetch('/user', { credentials: 'same-origin', redirect: 'error' })
+      .then(
+        res => this.setState({ auth: res.ok }),
+        err => console.error(err)
+      )
   }
 
   handleNavigate(page, event) {
@@ -17,37 +29,47 @@ class PageHeader extends Component {
 
   render() {
     const pages = ['Home', 'About', 'Learn', 'Search', 'Contact']
+    const logo =
+      <img className="m-2" width='64px' height='64px' src='/images/logo.svg' alt='Composites Recycling' />
+    const navigation =
+      <Nav className="p-2" pills>
+        {pages.map(page =>
+          <NavItem key={page}>
+            <NavLink href="#"
+                     onClick={this.handleNavigate.bind(this, page.toLowerCase())}
+                     active={this.props.page === page.toLowerCase()}>
+              {page}
+            </NavLink>
+          </NavItem>
+        )}
+      </Nav>
+    const toolbar =
+        <ButtonGroup>
+          <Button color="primary"
+                  active={this.props.page === 'create'}
+                  disabled={!this.state.auth}
+                  onClick={() => this.props.onNavigate('create')}>
+            <FontAwesomeIcon icon={CogIcon} />
+          </Button>
+          <Button color="primary"
+                  active={this.props.page === 'folder'}
+                  disabled={!this.state.auth}
+                  onClick={() => this.props.onNavigate('folder')}>
+            <FontAwesomeIcon icon={FolderIcon} />
+          </Button>
+          <Button color="primary"
+                  onClick={() => location.href = this.state.auth ? '/logout' : '/login'}>
+            <FontAwesomeIcon icon={this.state.auth ? UserTimesIcon : UserIcon} />
+          </Button>
+        </ButtonGroup>
     return (
       <header>
         <div className="d-flex align-items-center">
-          {/* Logo */}
-          <img className="m-2" width='64px' height='64px' src='/images/logo.svg' alt='Composites Recycling' />
-          {/* Navigation */}
-          <Nav className="p-2" pills>
-            {pages.map(page =>
-              <NavItem key={page}>
-                <NavLink href="#"
-                         onClick={this.handleNavigate.bind(this, page.toLowerCase())}
-                         active={this.props.page === page.toLowerCase()}>
-                  {page}
-                </NavLink>
-              </NavItem>
-            )}
-          </Nav>
-          {/* Buttons */}
-          <ButtonGroup className="p-2 ml-auto">
-            <Button color="primary" active={this.props.page === 'create'}
-                    onClick={() => this.props.onNavigate('create')}>
-              <FontAwesomeIcon icon={CogIcon} />
-            </Button>
-            <Button color="primary" active={this.props.page === 'folder'}
-                    onClick={() => this.props.onNavigate('folder')}>
-              <FontAwesomeIcon icon={FolderIcon} />
-            </Button>
-            <Button color="primary">
-              <FontAwesomeIcon icon={UserIcon} />
-            </Button>
-          </ButtonGroup>
+          {logo}
+          {navigation}
+          <div className="p-2 ml-auto">
+            {toolbar}
+          </div>
         </div>
         <hr />
       </header>
